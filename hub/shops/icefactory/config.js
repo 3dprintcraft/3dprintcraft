7cdfa741a86e3292a3700a2810eecf6ba6d2e88e
nomikab <95500061+nomikab@users.noreply.github.com>
2025-12-23 00:22:28 +0200

    Update config.js

--- hub/shops/icefactory/config.js
+++ hub/shops/icefactory/config.js
@@ -114,7 +114,7 @@ panel: {
 
     background: {
       type: "solid", // solid | gradient | image
-      color: "#F8FAFC",
+      color: "#d4e4e4",
       gradient: "linear-gradient(135deg,#ffffff,#f1f5f9)",
       image: {
         url: "",
